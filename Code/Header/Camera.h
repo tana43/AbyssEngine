9a4dd66960c24275d1b981b0d6674215a6347ab3
DESKTOP-S2ELMGH\tana6 <tana61143@gmail.com>
2024-05-25 15:41:52 +0900

    全然すすんでねーーーーーーーー

--- Code/Header/Camera.h
+++ Code/Header/Camera.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include "Component.h"
 #include "MathHelper.h"
+#include "SystemFunction.h"
 
 namespace AbyssEngine
 {
@@ -25,8 +26,14 @@ namespace AbyssEngine
         Vector3 ConvertTo2DVectorFromCamera(const Vector2& v);//XZ軸のみ計算
         Vector3 ConvertTo3DVectorFromCamera(const Vector2& v);//Y軸も計算
 
+    public:
+        void SetViewTarget(Transform* t) { viewTarget_ = t; }
+        [[nodiscared]] Transform* GetViewTarget() { return viewTarget_; }
+
     private:
         void DebugCameraController();
+
+        void CameraLagUpdate();
 
     public:
         float fov_ = DirectX::XMConvertToRadians(60.0f);
@@ -43,16 +50,22 @@ namespace AbyssEngine
 
         bool isMainCamera_ = false;
 
+        Vector3 socketOffset_;//Viewのオフセット座標
+        Vector3 targetOffset_;//Focusのオフセット座標
+        float armLength_;//カメラからターゲットまでの距離
+
+        bool enableCameraLag_ = true;//ターゲットへのカメラの追従を遅延させる
+        float cameraLagSpeed_ = 10.0f;//カメラの追従速度
+        CONSTANT_FLOAT Camera_Lag_Max_Distance = 3.0f;//カメラが遅延することができる最大距離
+
+
         friend class RenderManager;
 
     private: 
-        float debCameraSpeed_ = 10.0f;//デバッグ中のカメラスピード
-        bool enableDebugController_ = true;//デバッグ操作オン、オフ
+        float debCameraSpeed_       = 10.0f;//デバッグ中のカメラスピード
+        bool enableDebugController_ = true; //デバッグ操作オン、オフ
 
-        Transform viewTarget_;//ターゲット
-        Vector3 offsetPosition_;//ターゲットオフセット座標
-        Vector3 socketOffset_;//Viewのオフセット座標
-        Vector3 targetOffset_;//Focusのオフセット座標
+        Transform* viewTarget_;             //ビューターゲット
     };
 }
 
--- Code/Header/SystemFunction.h
+++ Code/Header/SystemFunction.h
@@ -1,6 +1,16 @@
 #pragma once
 #include <string>
 
+//デバッグ時のみ定数もImGuiでの変更が加えられるようにしておく
+#if _DEBUG
+typedef float CONSTANT_FLOAT;
+typedef int CONSTANT_INT;
+#else
+typedef constexpr float CONSTANT_FLOAT;
+typedef constexpr int CONSTANT_INT;
+#endif // DEBUG
+
+
 class SystemFunction
 {
 public:
